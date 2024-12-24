c5bbd8fd95db579a9050307fd807303baf122aac
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-12-24 10:46:24 -0500

    Sets up chakra provider ui

--- app/page.js
+++ app/page.js
@@ -1,10 +1,22 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Flex, Box, Text, Button } from '@chakra-ui/react';
+import { Flex, Box, Text, Button} from '@chakra-ui/react';
+
+
+const Banner = ({ purpose }) => {
+  return (
+    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+      {purpose}
+    </Flex>
+  );
+};
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-
-      </div>
+    <div>
+      <h1>Hello World</h1>
+      <Banner purpose={"For Sale"} />
+      <Banner purpose={"For Rent"} />
+    </div>
   );
 }

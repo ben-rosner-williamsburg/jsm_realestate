605fb221e2861aaef51a87e8701171b20d1d543f
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-12-24 11:09:50 -0500

    Add Box and flex containers

--- app/page.js
+++ app/page.js
@@ -21,8 +21,7 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <Box>
       <Banner 
       purpose="RENT A HOME"
       title1="Rental Homes for"
@@ -33,6 +32,9 @@ export default function Home() {
       linkName="/search?purpose=for-rent"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
+      <Flex flexWrap="wrap">
+        {/* Fetch the properties and map over them */}
+      </Flex>
       <Banner 
       purpose="BUY A HOME"
       title1="Find, Buy & Own Your"
@@ -43,6 +45,7 @@ export default function Home() {
       linkName="/search?purpose=for-sale"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-    </div>
+      {/* Fetch the properties and map over them */}
+    </Box>
   );
 }

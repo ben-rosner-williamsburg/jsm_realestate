2e6b715c957a3e12751e6cfd49a101d24c0e4a97
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2025-01-13 11:52:33 -0500

    Change currency to USD

--- components/Property.jsx
+++ components/Property.jsx
@@ -21,7 +21,7 @@ const Property = ({
           <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
             <Flex alignItems="center">
               <Box paddingRight="3" color="green.400">{isVerified && <GoVerified/> }</Box>
-              <Text fontWeight="bold" fontSize="lg">AED {millify(price)}{rentFrequency && `/${rentFrequency}`} </Text>
+              <Text fontWeight="bold" fontSize="lg">USD {millify(price)}{rentFrequency && `/${rentFrequency}`} </Text>
             </Flex>
             <Box>
               <Avatar size='sm' src={agency?.logo?.url} />
